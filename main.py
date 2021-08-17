b440dcfe2435bd4b6f3bc452ba9513b158dca524
Guy <3strategy@gmail.com>
2021-08-17 17:25:12 +0300

    same code. still going hard on the servo at initialization

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ import time
 a = pyfirmata.Arduino('COM5')
 
 pin=a.get_pin('d:13:o')  # here setting pin to be digital number 13, and setting it as OUTPUT.
+print('in 5 secs will set the servo pin')
+time.sleep(5)
+print('setting the servo pin')
 servo=a.get_pin('d:9:s')  # this will set pin 9 to be used as a servo
+                          # but it also moves the servo to position 0 (not every servo is happy with this)
+time.sleep(5)
 
 #this controls the servo (servo is set to a designated position (usually between 0 to 180 or a bit less).
 def serpos(ser,pos,sleep=1):
@@ -22,9 +27,12 @@ serpos(servo, 60)
 while True:  # main arduino loop
     print('start of loop', k)
     serpos(servo,130)
-    serpos(servo,60)
-    serpos(servo,15)
-    serpos(servo,60,6)  # sleeping 6 seconds
+    serpos(servo,180)
+    serpos(servo,200) # this behaves just like 180
+    serpos(servo,170)
+    serpos(servo,70)
+    serpos(servo,10)  # my servo does not like the 0 position...
+    serpos(servo,70,6)  # sleeping 6 seconds
     k+=1
 
 
