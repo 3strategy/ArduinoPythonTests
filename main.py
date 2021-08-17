420c1c560f8c9e7bdcd14c2caf0db28152c9814d
Guy <3strategy@gmail.com>
2021-08-17 16:42:12 +0300

    working python Servo example

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-#FIRST you must upload the firmata examples onto the arduino using the IDE.
-#This program relies on the serial connection. you cannot run the serial monitor from the IDE in parallel.
-#once stopped the arduino will stop (and continue listening).
-import pyfirmata as fir
+# FIRST you must upload the firmata example onto the arduino using the Arduino IDE.
+import pyfirmata
 import time
 
-a = fir.Arduino('COM5')
+a = pyfirmata.Arduino('COM5')
 
-#this example would blink the internal led
-while True:
-    a.digital[13].write(1)
-    time.sleep(1)
-    a.digital[13].write(0)
-    time.sleep(0.5)
+pin=a.get_pin('d:13:o')  # here setting pin to be digital number 13, and setting it as OUTPUT.
+servo=a.get_pin('d:9:s')  # this will set pin 9 to be used as a servo
+
+#this controls the servo (servo is set to a designated position (usually between 0 to 180 or a bit less).
+def serpos(ser,pos,sleep=1):
+    ser.write(pos)
+    pin.write(1)
+    print(pos)
+    time.sleep(0.2)
+    pin.write(0)
+    time.sleep(sleep) # need to give the servo a bit more time...
+
+k=1
+serpos(servo, 60)
+
+while True:  # main arduino loop
+    print('start of loop', k)
+    serpos(servo,130)
+    serpos(servo,60)
+    serpos(servo,15)
+    serpos(servo,60,6)  # sleeping 6 seconds
+    k+=1
+
+
+
+
+
+
